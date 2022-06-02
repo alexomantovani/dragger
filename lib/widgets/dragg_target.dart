35b54de63b5bcfa72a771b66205904fee84547e1
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-02 16:48:06 -0300

    create a StatefulWidget DraggTarget that returns a DragTarget<GenericProduct> and passes the GenericProduct data to the method saveCartProducts() in the DbWherehouse class

--- lib/widgets/dragg_target.dart
+++ lib/widgets/dragg_target.dart
@@ -1,16 +1,57 @@
 //Import Material package.
+import 'package:dragger/data/db_wherehouse.dart';
+import 'package:flutter/material.dart';
 //Import Provider package.
+import 'package:provider/provider.dart';
+
+//Import GenericProduct class
+import '/ui/components/generic_product.dart';
 
 //Criar um statefulWidget called DraggTarget.
+class DraggTarget extends StatefulWidget {
+  const DraggTarget({Key? key}) : super(key: key);
 
-//Inside the DraggTarget state create a DbWherehouse variable and initialize it with DbWherehouse() called dB.
+  @override
+  State<DraggTarget> createState() => _DraggTargetState();
+}
 
-//Create a didChageDependecies() method and initialize dB with the Provider package.
+class _DraggTargetState extends State<DraggTarget> {
+  //Inside the DraggTarget state create a DbWherehouse variable and initialize it with DbWherehouse() called dB.
+  DbWherehouse dB = DbWherehouse();
 
-//Inside the build method return a DragTarget widget of DragTarget<GenericProduct>().
-//In its builder function proportie check if the dB.cartProducts isEmpty, if it is empty,
-//return only the supermarket_cart picture. If it is not empty, return a Stack widget
-//with the supermarket_cart on top and the dB.cartProducts above.
-//In its onAccept: (data){} function property call the setState method with the dB.getCartProducts(data).
-//And then call the showDialog<void>() method for Android and showCupertinoDialog<void>() for iOS that will
-//be in its respective files.
+  //Create a didChageDependecies() method and initialize dB with the Provider package.
+  @override
+  void didChangeDependencies() {
+    dB = Provider.of<DbWherehouse>(context);
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Inside the build method return a DragTarget widget of DragTarget<GenericProduct>().
+    return DragTarget<GenericProduct>(
+      builder: (context, candidateData, rejectedData) {
+        //In its builder function proportie check if the dB.cartProducts isEmpty, if it is empty,
+        //return only the supermarket_cart picture. If it is not empty, return a Stack widget
+        //with the supermarket_cart on top and the dB.cartProducts above.
+        if (dB.cartProdcuts.isEmpty) {
+          return SizedBox(
+            child: Image.asset(''),
+          );
+        } else {
+          //In its onAccept: (data){} function property call the setState method with the dB.getCartProducts(data).
+          //And then call the showDialog<void>() method for Android and showCupertinoDialog<void>() for iOS that will
+          //be in its respective files.
+          return Stack(
+            children: const [],
+          );
+        }
+      },
+      onAccept: (productData) {
+        setState(() {
+          dB.saveCartProducts(productData);
+        });
+      },
+    );
+  }
+}
