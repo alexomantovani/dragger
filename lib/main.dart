bc9291f593edad11ab7b4f6631055bda3f371922
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-02 16:42:45 -0300

    add a ChageNotifierProvider and create a listener to the DbWherehouse class to the whole app via main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+//Import the Material package
 import 'package:flutter/material.dart';
 //Import the Provider package
+import 'package:provider/provider.dart';
 
+//Import DbWherehouse and DirectorySectionScreen
+import '/data/db_wherehouse.dart';
 import 'ui/screens/directory_section_screen.dart';
 
 void main() {
@@ -14,8 +18,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: DirectorySectionScreen(),
+    return ChangeNotifierProvider(
+      create: (context) => DbWherehouse(),
+      child: const MaterialApp(
+        home: DirectorySectionScreen(),
+      ),
     );
   }
 }
