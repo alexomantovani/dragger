9fea49c21893d7b916293b2581fd6e5040678c8d
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-03 10:46:46 -0300

    add the provider package, create the dB final variable to get the getProducts() method. Create the Category final arguments variable that receive arguments route settings from DirectorySectionScreen

--- lib/ui/screens/aisle_section_screen.dart
+++ lib/ui/screens/aisle_section_screen.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '/data/db_wherehouse.dart';
 
 class AisleSectionScreen extends StatelessWidget {
   const AisleSectionScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final dB = Provider.of<DbWherehouse>(context);
+    final Category arguments =
+        ModalRoute.of(context)!.settings.arguments as Category;
     return Scaffold(
       body: LayoutBuilder(
         builder: (context, constraints) {
-          final double width = constraints.maxWidth;
-          final double height = constraints.maxWidth;
-          return Column();
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    dB.getProducts(arguments, index);
+                    return dB.genericProduct.productWidget;
+                  },
+                ),
+              ),
+            ],
+          );
         },
       ),
     );
