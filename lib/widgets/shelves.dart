2fb3a119f155b3003d950ac2cf98582ccdaa3170
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-06 20:59:31 -0300

    create the pseudocode of the Shelves widget

---
+++ lib/widgets/shelves.dart
@@ -0,0 +1,10 @@
+//Import the Material package
+
+//Create a StatelessWidget called Shelves
+
+//inside the build() method return a Scaffold() without an AppBar()
+//The Scaffold() will get a Container() in it's body
+//The Container() will be the backdrop part of the shelve and will get a Column() as a child
+//The Column() will receive 10 children
+//The children will be 5 horizontal Container() widgets in perspective with the use of the Transform()
+//The other children will be 5 vertical Container() widgets in perspective with the use of the Transform()
