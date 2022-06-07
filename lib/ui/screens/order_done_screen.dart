71c558b0f2c084c3761afc788cc61ca02afa3a25
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-07 09:19:46 -0300

    create the pseudocode of the OrderDoneScreen()

---
+++ lib/ui/screens/order_done_screen.dart
@@ -0,0 +1,19 @@
+//Import the Material package
+
+//Import the Provider package
+
+//Import the DbWherehouse class
+
+//Create a StatefulWidget called OrderDoneScreen
+
+//Inside of it's build() method return a Scaffold() widget with an AppBar()
+
+//The AppBar() will get a title of "Detalhes" and a actions Text() widget
+//Displaying "Ajuda"
+
+//The Scaffold's body will return a Column() with 3 children
+//The first child will be a ListTile() with a done checkmark Icon and a Text() widget
+//Displaying "Pedido finalizado em: horário"
+//The second child will be an Image.asset() widget with the delivery address image 
+//The last child will be an ElevatedButton() widget "Recomeçar" that triggers the 
+//function getInitialState() in the DbWherehouse class
