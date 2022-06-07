ddbb4a6d492ce7f515cdc3243c7663c89aeba346
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-06 21:18:48 -0300

    create the pseudocode of the TileLinesGenerator class funciton

---
+++ lib/ui/components/tile_lines_generator.dart
@@ -0,0 +1,7 @@
+//import the Material package if needed
+
+//Create the TileLinesGenerator class
+
+//Inside the class create a function that gets constraints ans input and generates
+//Container() widgets as lines as output
+//This function uses for loops, if and case statements.
