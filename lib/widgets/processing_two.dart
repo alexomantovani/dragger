5a7392b38ee99e20014e98dbf1c5ea712c26e75d
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-07 09:52:21 -0300

    create the pseudocode of the ProcessingTwo widget

---
+++ lib/widgets/processing_two.dart
@@ -0,0 +1,16 @@
+//Import the Material package
+
+//Create a StatelessWidget called ProcessingTwo
+
+//Inside it's build() method return a Column() with 5 children
+
+//The first child will be a Row() widget with two other widgets
+//A Text() widget to display "Entrega" and a TextButton() to display "Retirada",
+//The TextButton() will trigger a ModalBottomSheet that displays the store address information
+
+//The second child will be a Text() widget "Onder deixar?" followed bellow by the third child, the ListView() widget
+//displaying chips with the possible places to choose.
+
+//The fourth child will be a Text() widget "Hoje, tempo para entrega" followed bellow by the fifth child, the ListView() widget
+//displaying chips with the possible places to choose.
+
