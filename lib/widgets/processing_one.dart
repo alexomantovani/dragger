79a21002cc60b83069aa919c2af05bcef42221dd
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-07 09:33:32 -0300

    create the pseudocode of the ProcessingOne widget

---
+++ lib/widgets/processing_one.dart
@@ -0,0 +1,12 @@
+//Import the Material package
+
+//create a StatelessWidget called ProcessingOne
+
+//Inside it's build() method return a Column() with 2 children
+
+//The first child will be a ListTile() widget with to other widgets
+//A CircleAvatar() widget to display the store logo and a Text() widget in the title
+//To display the store name
+
+//The second child will be a Expanded() widget with a ListView as a child that will return
+//Several ListTile() widgets with the orderSummary
