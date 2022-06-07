f2fd67517a2668e15ec57c8cf4e214c1c766bb31
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-07 09:54:59 -0300

    create the pseudocode of the ProcessingThree widget

---
+++ lib/widgets/processing_three.dart
@@ -0,0 +1,11 @@
+//Import the Material package
+
+//Create a StatelessWidget called ProcessingTwo
+
+//Inside it's build() method return a Column() with 3 children
+
+//The first child will be a Rbe a Expanded() widget with a ListView as a child that will return
+//Several ListTile() widgets with the orderSummary
+
+//The second child will be a Text() widget "Pagamento" followed bellow by the third child, the ListTile() widget
+//displaying the credit card company and card information.
