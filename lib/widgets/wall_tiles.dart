f4a59e4736b782d2dc7ad57b196af6c5c18f9812
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-06 21:11:51 -0300

    create the pseudocode of the WallTile widget

---
+++ lib/widgets/wall_tiles.dart
@@ -0,0 +1,10 @@
+//Import the Material package
+
+//Create a StatelessWidget called WallTiles()
+
+//Inside of it's build() method return a Scaffold() without an AppBar()
+//The Scaffold's body gets a Column()
+//The Column() will receive a generated number of Row() and Column() widgets
+//These Row() and Column() widgets will receive a generated number of Container() widgets
+//These Container() widgets will be the lines when put in perspective by the Transform() widget
+//Will finally form a Wall Tile
