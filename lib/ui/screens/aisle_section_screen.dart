f5c92a1216d4bc4ac0affdc67cf61c8c4d008ad6
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-08 14:56:08 -0300

    add the AppBar() and the functionality of it's buttons

--- lib/ui/screens/aisle_section_screen.dart
+++ lib/ui/screens/aisle_section_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,7 +13,118 @@ class AisleSectionScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final dB = Provider.of<DbWherehouse>(context);
     final arguments = ModalRoute.of(context)!.settings.arguments as Category;
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            showCupertinoDialog<String>(
+              context: context,
+              builder: (context) {
+                return CupertinoAlertDialog(
+                  content: const Text(
+                    'Deseja manter seus itens no carrinho?',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  actions: [
+                    CupertinoDialogAction(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Icon(
+                        Icons.thumb_down_alt,
+                        color: Colors.black,
+                      ),
+                    ),
+                    CupertinoDialogAction(
+                      onPressed: () {},
+                      child: const Icon(
+                        Icons.thumb_up_alt,
+                        color: Colors.black,
+                        size: 20.0,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        actions: [
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          color: Color(0xFF757575),
+                        ),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30.0),
+                              topRight: Radius.circular(30.0),
+                            ),
+                          ),
+                          height: size.height * 0.15,
+                          child: Column(
+                            children: const [
+                              Text(
+                                'Resumo de Valores',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                icon: const Icon(
+                  Icons.shopping_cart,
+                  color: Colors.black,
+                  size: 35.0,
+                ),
+              ),
+              Container(
+                transform: Matrix4.identity()
+                  ..translate(size.width * 0.06, -size.height * 0.015),
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.red,
+                ),
+                width: size.width * 0.04,
+                child: const Text(
+                  '3',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
       body: LayoutBuilder(
         builder: (context, constraints) {
           return Column(
