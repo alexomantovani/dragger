ec95d1a4d784aead0219bcf4c75ac201037be527
Alexandre Oliveira <alexomantovani@MacBook-Air-de-Alexandre.local>
2022-06-13 17:50:43 -0300

    delete tile_lines_generator.dart, floor_tiles.dart, shelves.dart and wall_tiles.dart files and widgets due to a change in the UX project

--- lib/ui/components/tile_lines_generator.dart
+++
@@ -1,7 +0,0 @@
-//import the Material package if needed
-
-//Create the TileLinesGenerator class
-
-//Inside the class create a function that gets constraints ans input and generates
-//Container() widgets as lines as output
-//This function uses for loops, if and case statements.
